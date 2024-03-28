1c1464399c0475e8e7e58cdbe0bb0448c3533663
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2024-03-28 19:41:13 -0300

    feature/add search bar

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -5,8 +5,8 @@ import { useEffect, useState } from "react";
 const graphQLClient = new GraphQLClient("https://rickandmortyapi.com/graphql");
 
 export const useFetch = (query: string) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState<any>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [data, setData] = useState<any>([]); //La data recibida por la API no esta tipada, Pues en un inicio se desconoce su contrato
   const [error, setError] = useState<string | unknown>();
 
   useEffect(() => {
