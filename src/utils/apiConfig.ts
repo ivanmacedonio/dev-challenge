5055aac640db981bb2582968251bca7246d11152
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2024-03-28 16:21:33 -0300

    change query to get all

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,11 +1,31 @@
 const apiQuery = `
 query {
   characters {
+    info {
+      count
+    }
+    results {
+      name
+    }
+  }
+  locations {
+    info {
+      count
+    }
+    results {
+      name
+    }
+  }
+  episodes {
+    info {
+      count
+    }
     results {
       name
     }
   }
 }
+
 `
 
 export default apiQuery;
